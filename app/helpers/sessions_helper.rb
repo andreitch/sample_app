f8100edc01a32b158b7aed5e7d4a910f4865f55a
Andrey Bekker <andreitch.top@gmail.com>
2020-04-02 14:08:49 +0000

    Remember tests

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -8,15 +8,15 @@ module SessionsHelper
   # Remembers a user in a persistent session.
   def remember(user)
     user.remember
-    cookies.permanent.signed[:user_id] = user.id
+    cookies.permanent.encrypted[:user_id] = user.id
     cookies.permanent[:remember_token] = user.remember_token
   end
   
-  # Returns the current logged-in user (if any
+  # Returns the user corresponding to the remember token cookie.
   def current_user
     if (user_id = session[:user_id])
       @current_user ||= User.find_by(id: user_id)
-    elsif (user_id = cookies.signed[:user_id])
+    elsif (user_id = cookies.encrypted[:user_id])
       user = User.find_by(id: user_id)
       if user && user.authenticated?(cookies[:remember_token])
         log_in user

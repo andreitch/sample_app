3c0c84d1df721e6025bff903f275007b50b99a22
Andrey Bekker <andreitch.top@gmail.com>
2020-04-04 13:16:07 +0000

    Deleting users

--- test/integration/users_edit_test.rb
+++ test/integration/users_edit_test.rb
@@ -22,7 +22,7 @@ class UsersEditTest < ActionDispatch::IntegrationTest
     get edit_user_path(@user)
     assert_equal session[:forwarding_url], edit_user_url(@user)
     log_in_as(@user)
-    assert_equal session[:forwarding_url], nil
+    assert_nil session[:forwarding_url]
     assert_redirected_to edit_user_url(@user)
     name = "Foo Bar"
     email = "foo@bar.com"

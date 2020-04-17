252ad339f3c199694c3f45a37cdbd8f1f604f652
Andrey Bekker <andreitch.top@gmail.com>
2020-04-17 13:21:14 +0000

    resolve prod host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,7 +91,7 @@ Rails.application.configure do
   
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'evening-ridge-20454.herokuapp.com'
+  host = 'microposts24.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :address => 'smtp.sendgrid.net',
